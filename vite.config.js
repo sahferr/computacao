8a81d5ae499e0c8b7e854188a5a85196d66b7cda
Sara <saferr.dev@gmail.com>
2026-06-15 13:03:27 -0300

    feat: correction path base vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/computopia/",
+  base: "/Computopia/",
   plugins: [react()],
 });
+z;
